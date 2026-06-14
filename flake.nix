10824f47b930ddf275b59d03f3b2e52af7ce4d52
toyama170402 <toyamasoujinn@gmail.com>
2026-06-14 16:02:28 +0900

    `/slides`にディレクトリと`main.typ`を置いたらpackageに追加されるよう変更 typstパッケージ列挙を別ファイルに分離 スライドが増やすたびにNix式を記述する必要をなくすため

--- flake.nix
+++ flake.nix
@@ -18,55 +18,36 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        typixPkg = typix.lib.${system};
         fontPaths = [
-            "${pkgs.inter}"
-            "${pkgs.texlivePackages.haranoaji}"
+          "${pkgs.inter}"
+          "${pkgs.texlivePackages.haranoaji}"
         ];
+
+        slideDir = ./slides;
+        allEntries = builtins.attrNames (builtins.readDir slideDir);
+        typstDirs = builtins.filter (
+          d: builtins.pathExists (toString slideDir + "/" + d + "/main.typ")
+        ) allEntries;
+
+        makePackage =
+          d:
+          typixPkg.buildTypstProject {
+            src = slideDir + ("/" + d);
+            typstSource = "main.typ";
+            unstable_typstPackages = import ./typstPackage.nix;
+            fontPaths = fontPaths;
+          };
+
+        slidePkgs = builtins.listToAttrs (
+          map (d: {
+            name = d;
+            value = makePackage d;
+          }) typstDirs
+        );
       in
       {
-        packages."git" = typix.lib.${system}.buildTypstProject {
-          src = ./git/.;
-          typstSource = "main.typ";
-          unstable_typstPackages = [
-            {
-              name = "fletcher";
-              version = "0.5.8";
-              hash = "sha256-kKVp5WN/EbHEz2GCTkr8i8DRiAdqlr4R7EW6drElgWk=";
-            }
-            {
-              name = "cetz";
-              version = "0.3.4";
-              hash = "sha256-5w3UYRUSdi4hCvAjrp9HslzrUw7BhgDdeCiDRHGvqd4=";
-            }
-            {
-              name = "oxifmt";
-              version = "0.2.1";
-              hash = "sha256-8PNPa9TGFybMZ1uuJwb5ET0WGIInmIgg8h24BmdfxlU=";
-            }
-            {
-              name = "touying";
-              version = "0.7.4";
-              hash = "sha256-G7Z+7o6SQjRa63DCLXcSNAtzohrk4Kljk4pSb4rJfeU=";
-            }
-            {
-              name = "uniwarn";
-              version = "0.1.1";
-              hash = "sha256-alpI7IgUJfjxDy6KXlPGX2N9KMEHPms/pbxXHRJrmZw=";
-            }
-            {
-              name = "codly";
-              version = "1.3.0";
-              hash = "sha256-WcqvySmSYpWW+TmZT7TgPFtbEHA+bP5ggKPll0B8fHk=";
-            }
-            {
-              name = "codly-languages";
-              version = "0.1.1";
-              hash  = "sha256-TSp3unFhn3NSaWhWYZb/i3rD4OolbNZNTdQeBxJ4Jfs=";
-            }
-          ];
-          fontPaths = fontPaths;
-        };
-
+        packages = slidePkgs;
         devShells.default = typix.lib.${system}.devShell {
           fontPaths = fontPaths;
         };
---
+++ typstPackage.nix
@@ -0,0 +1,37 @@
+[
+  {
+    name = "fletcher";
+    version = "0.5.8";
+    hash = "sha256-kKVp5WN/EbHEz2GCTkr8i8DRiAdqlr4R7EW6drElgWk=";
+  }
+  {
+    name = "cetz";
+    version = "0.3.4";
+    hash = "sha256-5w3UYRUSdi4hCvAjrp9HslzrUw7BhgDdeCiDRHGvqd4=";
+  }
+  {
+    name = "oxifmt";
+    version = "0.2.1";
+    hash = "sha256-8PNPa9TGFybMZ1uuJwb5ET0WGIInmIgg8h24BmdfxlU=";
+  }
+  {
+    name = "touying";
+    version = "0.7.4";
+    hash = "sha256-G7Z+7o6SQjRa63DCLXcSNAtzohrk4Kljk4pSb4rJfeU=";
+  }
+  {
+    name = "uniwarn";
+    version = "0.1.1";
+    hash = "sha256-alpI7IgUJfjxDy6KXlPGX2N9KMEHPms/pbxXHRJrmZw=";
+  }
+  {
+    name = "codly";
+    version = "1.3.0";
+    hash = "sha256-WcqvySmSYpWW+TmZT7TgPFtbEHA+bP5ggKPll0B8fHk=";
+  }
+  {
+    name = "codly-languages";
+    version = "0.1.1";
+    hash = "sha256-TSp3unFhn3NSaWhWYZb/i3rD4OolbNZNTdQeBxJ4Jfs=";
+  }
+]
